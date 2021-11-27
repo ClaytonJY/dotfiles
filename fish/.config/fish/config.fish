8ff198e4cb7b6e5c9e11be90f46480d780d67260
Clayton Yochum <claytonjy@gmail.com>
2021-11-26 23:35:42 -0500

    Add pyenv configs to fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,4 +3,9 @@ if status is-interactive
     # starship.rs
     starship init fish | source
 
+
 end
+
+# pyenv
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
