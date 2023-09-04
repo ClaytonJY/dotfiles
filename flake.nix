77093fd92d8f1c51a42e9d79967c066a6efcb170
Clayton Yochum <claytonjy@gmail.com>
2023-09-04 10:56:34 -0600

    Remove a few more old comments.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
-  description = "Home Manager configuration of claytonjy";
+  description = "claytonjy's home-manager configuration";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -18,13 +17,7 @@
     {
       homeConfigurations."claytonjy" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
